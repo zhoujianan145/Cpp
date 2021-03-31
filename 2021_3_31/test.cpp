aa8595394c5607f20a948792ee3be3db0c683b43
zhoujianan145 <972935804@qq.com>
2021-03-31 21:25:14 +0800

    string最终实现（一）差两个全局重载比较函数

---
+++ 2021_3_31/test.cpp
@@ -0,0 +1,28 @@
+#include"string.h"
+
+using namespace zjn;
+int main()
+{
+	zjn::string s = "12345";
+	zjn::string s1(s);
+	string s2 = "zj";
+	s1 = s2;
+	
+	s1.push_back('x');
+	s1.append("hello");
+	s1 += "bi";
+	s1 += "t";
+	s1 += s;
+	s.insert(0,"");
+	s1.erase(4);
+	
+	int n=s1.find('x');
+	bool a= s1 > s2;
+	bool b= s1 == s2;
+
+	string s4;
+	std::cin >> s4 ;
+	s4.resize(5);
+	std::cout << s4 << std::endl;
+	return 0;
+}
